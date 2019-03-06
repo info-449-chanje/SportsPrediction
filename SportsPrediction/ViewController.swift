8e3115225690c8f5910bbbd20ddc55e733758945
NickHytrek <nshytrek@gmail.com>
2019-03-05 19:16:13 -0800

    initial commit

---
+++ SportsPrediction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsPrediction
+//
+//  Created by Nicholas S. Hytrek on 3/5/19.
+//  Copyright © 2019 INFO 449 CHANGJE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
