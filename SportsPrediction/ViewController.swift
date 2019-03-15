e6da67b4680e33e86eff7b38362b396e15b8edc7
NickHytrek <nshytrek@gmail.com>
2019-03-14 17:15:07 -0700

    configured project compatability with firebase, added signup option on login page

--- SportsPrediction/ViewController.swift
+++ SportsPrediction/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Nicholas S. Hytrek on 3/5/19.
 //  Copyright © 2019 INFO 449 CHANGJE. All rights reserved.
+//  TheRundown
 //  API key: d372d41f7fmshe392f01d3e7c6b0p13f111jsn1169de9473f3
 
 import UIKit
