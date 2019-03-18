83179a26bde12fa61f6a5ffbb508ec5573b4b7f3
Jacques DeBar <jacques.debar@gmail.com>
2019-03-17 17:43:35 -0700

    add piechart

---
+++ SportsPrediction/ProfileGraphViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileGraphViewController.swift
+//  SportsPrediction
+//
+//  Created by Jacques DeBar on 3/17/19.
+//  Copyright © 2019 INFO 449 CHANGJE. All rights reserved.
+//
+
+import UIKit
+
+class ProfileGraphViewController: UIViewController {
+
+  
+  @IBOutlet weak var graph: PieChartView!
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
