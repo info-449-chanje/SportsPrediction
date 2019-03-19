09dfa318aff427f92c32d957a4af76c3ad5323d5
NickHytrek <nshytrek@gmail.com>
2019-03-19 14:10:45 -0700

    added sorting to leaderboard

--- SportsPrediction/LeaderboardViewController.swift
+++ SportsPrediction/LeaderboardViewController.swift
@@ -13,8 +13,8 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
     
     @IBOutlet weak var leaderBoard: UITableView!
     var users = [String]()
-    var streaks = [Any]()
-    
+    var streaks = [Int]()
+    var combinedUserAndStreak : [String : Int] = [:]
     
     
     
@@ -24,7 +24,7 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "leaderboardCell")
-        cell!.textLabel?.text = "\(indexPath.row + 1).   \(users[indexPath.row])"
+        cell!.textLabel?.text = "\(indexPath.row + 1). Streak: \(streaks[indexPath.row])   -   User: \(users[indexPath.row])"
         return cell!
     }
     
@@ -53,15 +53,29 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
         }
         var i = 0
         while i < users.count {
-            streaks.append(data[users[i]]!["recordStreak"].unsafelyUnwrapped.unsafelyUnwrapped)
+            let rawData = data[users[i]]!["recordStreak"].unsafelyUnwrapped.unsafelyUnwrapped
+            let rawToString = String(describing: rawData)
+            let rawAsInt = Int(rawToString)
+            streaks.append(rawAsInt ?? 0)
             i = i + 1
         }
-        print(streaks)
-        
-//        let a = data[users[0]]!["recordStreak"].unsafelyUnwrapped.unsafelyUnwrapped
-//        print(a)
-//
-        
+        for (index, element) in users.enumerated() {
+            combinedUserAndStreak[element] = streaks[index]
+        }
+        let sortedDict = combinedUserAndStreak.sorted(by: { $0.value > $1.value})
+        combinedUserAndStreak.removeAll()
+        var j = 0
+        while j < sortedDict.count {
+            combinedUserAndStreak[sortedDict[j].key] = sortedDict[j].value
+            j = j + 1
+        }
+        users.removeAll()
+        streaks.removeAll()
+        for (key, value) in combinedUserAndStreak {
+            users.append(key)
+            streaks.append(value)
+        }
+        print(users, streaks)
         DispatchQueue.main.async {
             self.leaderBoard.reloadData()
         }
