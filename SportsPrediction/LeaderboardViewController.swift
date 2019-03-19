d1050617bb1314236f39859171e3afd80243407b
NickHytrek <nshytrek@gmail.com>
2019-03-18 23:04:06 -0700

    read user names to leaderboard unsorted

--- SportsPrediction/LeaderboardViewController.swift
+++ SportsPrediction/LeaderboardViewController.swift
@@ -7,16 +7,65 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class LeaderboardViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var leaderBoard: UITableView!
+    var users = [String]()
+    var streaks = [Any]()
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leaderboardCell")
+        cell!.textLabel?.text = users[indexPath.row]
+        return cell!
     }
     
 
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        leaderBoard.dataSource = self
+        leaderBoard.delegate = self
+        createTopUsers(completion: self.completionHandler)
+        // Do any additional setup after loading the view.
+    }
+    
+    func createTopUsers(completion: @escaping ([String: AnyObject]) -> Void) {
+        let ref = Database.database().reference()
+        ref.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as! [String: AnyObject]
+            completion(value)
+        })
+    }
+    
+    func completionHandler(data: [String: AnyObject]) {
+        for (key, value) in data {
+            users.append(key)
+        }
+        var i = 0
+        while i < users.count {
+            streaks.append(data[users[i]]!["recordStreak"].unsafelyUnwrapped.unsafelyUnwrapped)
+            i = i + 1
+        }
+        print(streaks)
+        
+//        let a = data[users[0]]!["recordStreak"].unsafelyUnwrapped.unsafelyUnwrapped
+//        print(a)
+//
+        
+        DispatchQueue.main.async {
+            self.leaderBoard.reloadData()
+        }
+    }
     @IBAction func buttonEvent(_ sender: UIButton) {
         self.performSegue(withIdentifier: "LeaderboardToEvent", sender: self)
     }
@@ -25,5 +74,5 @@ class LeaderboardViewController: UIViewController {
         self.performSegue(withIdentifier: "LeaderboardToProfile", sender: self)
     }
     
-
+    
 }
